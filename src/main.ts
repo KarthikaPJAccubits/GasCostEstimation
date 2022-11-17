93a1a8dabe5efa6bb3ee58503c41c0b07f3ac38e
KarthikaPJAccubits <karthika.pj@accubits.com>
2022-11-17 14:02:38 +0530

    main.ts edited

--- src/main.ts
+++ src/main.ts
@@ -87,9 +87,7 @@ async function main() {
 
 
 
-                function sleep(ms: number) {
-                    return new Promise(resolve => setTimeout(resolve, ms));
-                }
+               
             })
     })
 
